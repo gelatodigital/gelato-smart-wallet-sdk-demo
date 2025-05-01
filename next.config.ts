c52ba6316dde20d906dc01197868cbe789b7a896
HiteshMittal07 <smartyboy.hm15@gmail.com>
2025-05-01 20:45:55 +0530

    feat: Intialized sdk version update

--- next.config.ts
+++ next.config.ts
@@ -8,32 +8,6 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 31536000,
     domains: ["lh3.googleusercontent.com"],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@gelatomega/core": path.resolve(
-        __dirname,
-        "../../packages/core/src/index.ts"
-      ),
-      "@gelatomega/react-dynamic": path.resolve(
-        __dirname,
-        "../../packages/react-dynamic/src/index.ts"
-      ),
-      "@gelatomega/react-privy": path.resolve(
-        __dirname,
-        "../../packages/react-privy/src/index.ts"
-      ),
-      "@gelatomega/react-sdk": path.resolve(
-        __dirname,
-        "../../packages/react-sdk/src/index.ts"
-      ),
-      "@gelatomega/core/oracle": path.resolve(
-        __dirname,
-        "../../packages/core/src/oracle/index.ts"
-      ),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
